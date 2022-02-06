78d966589534d7f88129a83664c7e93221a21894
KandilKhaled <khaled.kandil@stud.fra-uas.de>
2022-02-06 17:30:43 +0100

    Adding New Video

--- Project12_HTMCLAVideoLearning/HTMVideoLearning/HTMVideoLearning/VideoLearning.cs
+++ Project12_HTMCLAVideoLearning/HTMVideoLearning/HTMVideoLearning/VideoLearning.cs
@@ -40,8 +40,8 @@ namespace HTMVideoLearning
             {
                 Directory.CreateDirectory(testOutputFolder);
             }
-            int frameWidth = 40;
-            int frameHeight = 40;
+            int frameWidth = 30;
+            int frameHeight = 30;
             ColorMode colorMode = ColorMode.BLACKWHITE;
             double frameRate = 12;
             // adding condition for 
@@ -435,7 +435,7 @@ namespace HTMVideoLearning
             }
 
             // Video Parameter 
-            int frameWidth = 40;
+            int frameWidth = 30;
             int frameHeight = 40;
             ColorMode colorMode = ColorMode.BLACKWHITE;
             double frameRate = 10;
