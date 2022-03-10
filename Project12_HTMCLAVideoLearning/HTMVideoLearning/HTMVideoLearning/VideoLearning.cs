cb342489eca9c30ae4d1fc44a779cb90d97552e6
Nghia Nguyen <nghianguyentuan15@gmail.com>
2022-03-10 15:44:54 +0100

    HTM config result

--- Project12_HTMCLAVideoLearning/HTMVideoLearning/HTMVideoLearning/VideoLearning.cs
+++ Project12_HTMCLAVideoLearning/HTMVideoLearning/HTMVideoLearning/VideoLearning.cs
@@ -790,7 +790,7 @@ namespace HTMVideoLearning
 
                 MaxBoost = 10.0,
                 DutyCyclePeriod = 25,
-                //MinPctOverlapDutyCycles = 0.75,
+                MinPctOverlapDutyCycles = 1.5,
                 MaxSynapsesPerSegment = (int)(0.02 * numColumns[0]), 
 
                 ActivationThreshold = 15,
